f7e65577dd34973b60164ac21d45377e4f13e6f6
sudarsanamrithika <veethe2nd@gmail.com>
2024-05-02 19:36:58 +0800

    Complete 1-4

---
+++ app/src/main/java/sg/edu/np/mad/practical3/ListActivity.java
@@ -0,0 +1,69 @@
+package sg.edu.np.mad.practical3;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_list);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ImageView imageProfile = findViewById(R.id.foreground);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Profile");
+        builder.setMessage("MADness");
+        builder.setCancelable(false);
+        builder.setPositiveButton("View", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                int id = new Random().nextInt(999999);
+                Intent MainActivity = new Intent(ListActivity.this, MainActivity.class);
+                MainActivity.putExtra("id",id);
+                startActivity(MainActivity);
+            }
+        });
+        builder.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        imageProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
+    }
+
+
+
+
+
+}
--- app/src/main/java/sg/edu/np/mad/practical3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/practical3/MainActivity.java
@@ -8,7 +8,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.QuickContactBadge;
+import android.widget.TextView;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,37 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Initialize a new User object
+        User user = new User("John Doe","MAD Developer", 1, false);
+
+        // Get the TextViews and Button from the layout
+        TextView tvName = findViewById(R.id.textView);
+        TextView tvDescription = findViewById(R.id.textView2);
+        Button btnFollow = findViewById(R.id.button);
+
+//        // Get the TextViews with the User's name, description and default button message
+        tvName.setText(user.name);
+        tvDescription.setText(user.description);
+        btnFollow.setText("FOLLOW");
+
+        btnFollow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if (user.followed) {
+                    btnFollow.setText("FOLLOW");
+                    user.followed = false;
+                    Toast.makeText(MainActivity.this, "Unfollowed", Toast.LENGTH_SHORT).show();
+                } else {
+                    btnFollow.setText("UNFOLLOW");
+                    user.followed = true;
+                    Toast.makeText(MainActivity.this, "Followed", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+
+        });
+
     }
+
 }
