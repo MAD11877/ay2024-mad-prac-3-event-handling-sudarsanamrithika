4958c69cc0efa52297ffda1cd36b7a05623bcff2
sudarsanamrithika <veethe2nd@gmail.com>
2024-05-02 19:54:22 +0800

    complete practical

--- app/src/main/java/sg/edu/np/mad/practical3/ListActivity.java
+++ app/src/main/java/sg/edu/np/mad/practical3/ListActivity.java
@@ -4,11 +4,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AlertDialog;
